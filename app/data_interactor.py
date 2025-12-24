95d6d38e3d686ef89a9e3eb2c7844d7e359eb37d
Y-Klein <k0556714698@gmail.com>
2025-12-24 17:29:39 +0200

    add try excepting and env file

--- app/data_interactor.py
+++ app/data_interactor.py
@@ -22,14 +22,18 @@ class Contact(BaseModel):
 
 
     def get_dict(self):
-        return {"id":self.id,"first name":self.first_name,"last name":self.last_name,"phone number":self.phone_number}
+        return {"id":self.id,"first name":self.first_name,
+                "last name":self.last_name,"phone number":self.phone_number}
 
 def create_contact(contact:Contact):
     contact = Contact.get_dict(contact)
     with connection.cursor() as cursor:
-        cursor.execute("INSERT INTO contacts (first_name, last_name, phone_number) VALUES (%s,%s,%s);",
-                       [contact["first name"],contact["last name"],contact["phone number"]])
-        cursor.execute("SELECT id FROM contacts WHERE phone_number = %s ",[contact["phone number"]])
+        cursor.execute(
+            "INSERT INTO contacts (first_name, last_name, phone_number) VALUES (%s,%s,%s);",
+            [contact["first name"],contact["last name"],contact["phone number"]])
+        cursor.execute(
+            "SELECT id FROM contacts WHERE phone_number = %s ",
+            [contact["phone number"]])
         result =  cursor.fetchall()[0][0]
         connection.commit()
         return result
@@ -41,7 +45,12 @@ def get_all_contacts():
         all_contacts = cursor.fetchall()
         result = []
         for my_object in all_contacts:
-            result.append(Contact.get_dict(Contact(id=my_object[0],first_name=my_object[1],last_name=my_object[2],phone_number=my_object[3])))
+            result.append(Contact.get_dict(Contact(
+                id=my_object[0],
+                first_name=my_object[1],
+                last_name=my_object[2],
+                phone_number=my_object[3]
+                )))
         return result
 
 def update_contact(my_id,change_place,new_value):
@@ -52,15 +61,9 @@ def update_contact(my_id,change_place,new_value):
 
 def delete_contact(my_id):
     with connection.cursor() as cursor:
-        cursor.execute(f"DELETE FROM contacts WHERE id = {my_id}")
+        cursor.execute("DELETE FROM contacts WHERE id = %s",[my_id])
         connection.commit()
         return True
 
 
 
-# a = Contact(first_name="hhh",last_name="bbb",phone_number="6660")
-# print(create_contact(a))
-# print(get_all_contacts())
-# print(update_contact(3,'phone_number' ,"6670" ))
-# print(delete_contact(3))
-# connection.close()
--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from http.client import HTTPException
+
 from fastapi import FastAPI
 from data_interactor import *
 
@@ -6,18 +8,30 @@ app = FastAPI()
 
 @app.get("/contacts")
 def get_all():
-    return get_all_contacts()
+    try:
+        return get_all_contacts()
+    except:
+        raise TypeError("Something went wrong")
 
 @app.post("/contacts")
 def create(contact:Contact):
-    return create_contact(contact)
+    try:
+        return create_contact(contact)
+    except:
+        raise TypeError("Something went wrong")
 
 @app.put("/contacts/{id}")
 def update(my_id,change_place,new_value):
-    return update_contact(my_id,change_place,new_value)
+    try:
+        return update_contact(my_id,change_place,new_value)
+    except:
+        raise TypeError("Something went wrong")
 
 @app.delete("/contacts/{id}")
 def delete(my_id):
-    return delete_contact(my_id)
+    try:
+        return delete_contact(my_id)
+    except:
+        raise TypeError("Something went wrong")
 
 
