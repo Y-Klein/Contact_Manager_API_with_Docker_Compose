fb666502e6421c4052c60b6279924926af02b98b
Y-Klein <k0556714698@gmail.com>
2025-12-24 10:27:56 +0200

    add 2 function:update & delete

--- app/data_interactor.py
+++ app/data_interactor.py
@@ -28,24 +28,35 @@ def create_contact(contact:Contact):
     with connection.cursor() as cursor:
         cursor.execute("INSERT INTO contacts (first_name, last_name, phone_number) VALUES (%s,%s,%s);",
                        [contact.first_name,contact.last_name,contact.phone_number])
-        cursor.execute("SELECT id FROM contacts WHERE first_name = %s ",[contact.first_name])
+        cursor.execute("SELECT id FROM contacts WHERE phone_number = %s ",[contact.phone_number])
         print(cursor.fetchall()[0][0])
         connection.commit()
 
 
 def get_all_contacts():
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM contacts")
+        cursor.execute(f"SELECT * FROM contacts")
         all_contacts = cursor.fetchall()
         for my_object in all_contacts:
             print(Contact(id=my_object[0],first_name=my_object[1],last_name=my_object[2],phone_number=my_object[3]))
 
+def update_contact(my_id,change_place,new_value):
+    with connection.cursor() as cursor:
+        cursor.execute(f"UPDATE contacts SET {change_place} = {new_value} WHERE id = {my_id}")
+        connection.commit()
+        return True
+
+def delete_contact(my_id):
+    with connection.cursor() as cursor:
+        cursor.execute(f"DELETE FROM contacts WHERE id = {my_id}")
+        connection.commit()
+        return True
 
 
 
-
-
-a = Contact(first_name="hhh",last_name="bbb",phone_number="6660")
+# a = Contact(first_name="hhh",last_name="bbb",phone_number="6660")
 # create_contact(a)
-get_all_contacts()
+# get_all_contacts()
+# print(update_contact(3,'phone_number' ,"6670" ))
+# print(delete_contact(3))
 connection.close()
